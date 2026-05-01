4c57571fc2bdb5bd4cbe06f653b10b818783b849
Preethy <207163661+preethykmoorthy@users.noreply.github.com>
2026-05-01 06:46:06 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        message = request.form["message"]
+        print(name, message)  # prints in terminal
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
